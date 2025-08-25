afeb92c5c9777763d1de42411302b9d09db0b3dc
MatveyMak22 <matveylet39@gmail.com>
2025-08-26 00:26:52 +0500

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,44 @@
+const CACHE_NAME = 'radio-player-v4';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/manifest.json',
+  '/1.jpg',
+  '/2.jpg',
+  '/logo.png',
+  '/live-cover.jpg',
+  '/default-cover.jpg'
+];
+
+// Установка Service Worker
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+// Активация Service Worker
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheName !== CACHE_NAME) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
+
+// Обработка запросов
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        return response || fetch(event.request);
+      })
+  );
+});
